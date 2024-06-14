8f0c5406dec1d8743df38964abade771f9f1e9cc
Artem <artem.andreyev212@gmail.com>
2024-06-14 22:04:48 +0300

    Added animation to first image

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import $ from"jquery";
 
+const img =  $("#1");
+const images = $("img");
+
 $(document).ready(function() {
     $(".list-item:first").hover(function() {
         $(this).toggleClass("active");
@@ -14,5 +17,30 @@ $(document).ready(function() {
             opacity: "toggle",
             height: "toggle",
         }, 2000);
+    });
+
+    // Image Animation
+
+    let imageAnimation;
+    img.addEventListener("click", () => {
+    if (!imageAnimation) {
+    imageAnimation = images[0].animate([
+            {transform: "translateY(0) rotate(0)",
+            filter: "opacity(100%)"},
+            {transform: "translateY(100px) rotate(180deg)",
+            filter: "opacity(50%)"},
+            {transform: "translateY(-100px) rotate(270deg)",
+            filter: "opacity(75%)"},
+            {transform: "translateY(0) rotate(360deg)",
+            filter: "opacity(100%)"}
+        ], {
+            duration: 3000,
+            iterations: Infinity
+        });
+    } else if (imageAnimation.playState === "paused") {
+        imageAnimation.play();
+    } else {
+        imageAnimation.pause();
+    }
     });
 });
